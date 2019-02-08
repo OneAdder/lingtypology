eb8255589048743fb635dd88e960843fa3d4cef4
OneAdder <mikivo@list.ru>
2019-02-09 01:02:29 +0300

    работает с Гариковыми кабардинацами, круто

--- typological_map.py
+++ typological_map.py
@@ -1,14 +1,14 @@
 import folium
 import pandas
 
-colors = ['#0000FF', '#8A2BE2', '#A52A2A', '#DEB887', '#5F9EA0', '#7FFF00', '#D2691E', '#FF7F50', '#6495ED']
+colors = ['#0000FF', '#8A2BE2', '#A52A2A', '#DEB887', '#5F9EA0', '#7FFF00', '#D2691E', '#FF7F50', '#6495ED', '#F08080']
 
 legend_html = '''
               <div style="position: fixed; 
                           bottom: 50px;
                           left: 50px;
                           width: 250px;
-                          height: 170px; 
+                          height: 500px; 
                           border:2px solid grey;
                           z-index:9999;
                           font-size:14px;
@@ -23,6 +23,14 @@ def get_affiliations(languages):
         affiliations.append(tuple(csv[csv.language == language].affiliation)[0])
     return affiliations
 
+
+class LingMapError(Exception):
+    def __init__(self,value):
+        self.msg = value
+    def __str__(self):
+        return self.msg
+
+
 class LingMap(object):
     def __init__(self, languages):
         self.languages = languages
@@ -30,16 +38,30 @@ class LingMap(object):
     def _get_coordinates(self, language):
         longitude = float(csv[csv.language == language].longitude)
         latitude = float(csv[csv.language == language].latitude)
-        return (longitude, latitude)
+        return (latitude, longitude)
 
     def _get_glot_id(self,language):
         return tuple(csv[csv.language == language].glottocode)[0]
 
     def add_features(self, features):
+        self._sanity_check(features, feature_name='features')
         self.features = features
 
     def add_popups(self, popups):
+        self._sanity_check(popups, feature_name='popups')
         self.popups = popups
+
+    def add_tooltips(self, tooltips):
+        self._sanity_check(features, feature_name='tooltips')
+        self.tooltips = tooltips
+
+    def add_custom_coordinates(self, custom_coordinates):
+        self._sanity_check(custom_coordinates, feature_name='custom_coordinates')
+        self.custom_coordinates = custom_coordinates
+
+    def _sanity_check(self, features, feature_name='corresponding lists'):
+        if len(self.languages) != len(features):
+            raise LingMapError("Length of languages and {} does not match".format(feature_name))
     
     def _create_map(self):
         m = folium.Map(location=[0, 0], zoom_start=2)
@@ -54,30 +76,29 @@ class LingMap(object):
             features = [mapping[f] for f in self.features]
 
         for i, language in enumerate(self.languages):
-            coordinates = self._get_coordinates(language)
+            if 'custom_coordinates' in dir(self):
+                coordinates = self.custom_coordinates[i]
+            else:
+                coordinates = self._get_coordinates(language)
             if 'features' in dir(self):
                 color = features[i]
             else:
                 color = '#DEB887'
-            if 'popups' in dir(self):
-                popup_href = '''<a href="https://glottolog.org/resource/languoid/id/{}" onclick="this.target='_blank';">{}</a><br>'''
-                popup = popup_href.format(self._get_glot_id(language), language) + self.popups[i]
-                folium.CircleMarker(
-                        location=[coordinates[1], coordinates[0]],
+            marker = folium.CircleMarker(
+                        location=[coordinates[0], coordinates[1]],
                         radius=5,
                         fill=True,
                         fill_opacity=1,
-                        color=color,
-                        popup=popup
-                    ).add_to(m)
-            else:
-                folium.CircleMarker(
-                            location=[coordinates[1], coordinates[0]],
-                            radius=5,
-                            fill=True,
-                            fill_opacity=1,
-                            color=color
-                        ).add_to(m)
+                        color=color
+                    )
+            if 'popups' in dir(self):
+                popup_href = '''<a href="https://glottolog.org/resource/languoid/id/{}" onclick="this.target='_blank';">{}</a><br>'''
+                popup = folium.Popup(popup_href.format(self._get_glot_id(language), language) + self.popups[i])
+                popup.add_to(marker)
+            if 'tooltips' in dir(self):
+                tooltip = folium.Tooltip(self.tooltips[i])
+                tooltip.add_to(marker)
+            marker.add_to(m)
         if 'features' in dir(self):
             legend += '</div>'
             m.get_root().html.add_child(folium.Element(legend))
@@ -88,14 +109,37 @@ class LingMap(object):
 
     def render(self):
         return self._create_map().get_root().render()
-    
 
-m = LingMap(["Adyghe", "Kabardian", "Polish", "Russian", "Bulgarian"])
+'''    
+languages = ["Adyghe", "Kabardian", "Polish", "Russian", "Bulgarian"]
+m = LingMap(languages)
 
-affs = get_affiliations(["Adyghe", "Kabardian", "Polish", "Russian", "Bulgarian"])
+affs = get_affiliations(languages)
 features = ["Agglutinative", "Agglutinative", "Inflected", "Inflected", "Analythic"]
 
 m.add_features(features)
 m.add_popups(affs)
+m.add_tooltips(languages)
+
+m.save('test_map.html')
+'''
+
+circassian = pandas.read_csv('circassian.csv', delimiter=',', header=0)
+
+coordinates = list(zip(list(circassian.latitude), list(circassian.longitude)))
+features = list(circassian.dialect)
+
+languages = list(circassian.language)
+popups = list(circassian.village)
+
+m = LingMap(languages)
+
+
+
+m.add_features(features)
+m.add_popups(popups)
+m.add_tooltips(languages)
+m.add_custom_coordinates(coordinates)
+
 m.save('test_map.html')
 
