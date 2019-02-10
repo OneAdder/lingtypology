ad6d43f4336a8d4cd5f4ec2d031431f8e9800643
OneAdder <mikivo@list.ru>
2019-02-10 14:31:25 +0300

    добавил stroke_features

--- typological_map.py
+++ typological_map.py
@@ -50,15 +50,51 @@ class LingMap(object):
                 popup = folium.Popup(self.popups[i])
                 popup.add_to(marker)
 
-    def _create_legend(self, m, legend_data):
+    def _create_legend(self, m, legend_data, position='right'):
         with open('legend.html', 'r', encoding='utf-8') as f:
             template = f.read()
         template = jinja2.Template(template)
-        template = template.render(data=legend_data)
+        template = template.render(data=legend_data, position=position)
         template = '{% macro html(this, kwargs) %}' + template + '{% endmacro %}'
         macro = branca.element.MacroElement()
         macro._template = branca.element.Template(template)
         m.get_root().add_child(macro)
+
+    def _set_marker(self,
+                    location,
+                    radius=7,
+                    fill=True,
+                    stroke=False,
+                    weight=1,
+                    fill_opacity=1,
+                    color='#000000',
+                    fill_color='#DEB887'):
+        marker = folium.CircleMarker(location=location, radius=radius, fill=fill, stroke=stroke, weight=weight, fill_opacity=fill_opacity, color=color, fill_color=fill_color)
+        return marker
+
+    def _prepare_features(self, features, stroke=False):
+        colors = self.colors
+        if stroke:
+            colors.reverse()
+        if self.numeric and not stroke:
+            features.sort()
+            colormap = branca.colormap.LinearColormap(colors=['#e6ccff','#4a008f'], index=[features[0],features[-1]], vmin=features[0], vmax=features[-1])
+            groups_colors = [(0, colormap(feature)) for feature in features]
+            data = colormap
+        else:
+            mapping = {}
+            clear_features = []
+            groups = []
+            data = ''
+            for i, feature in enumerate(features):
+                if feature not in clear_features:
+                    clear_features.append(feature)
+                    groups.append(folium.FeatureGroup(name=self.features[i]))
+            for i, feature in enumerate(clear_features):
+                mapping[feature] = (groups[i], colors[i])
+                data += '<li><span style="background: {};opacity:0.7;"></span>{}</li>\n'.format(self.colors[i], feature)
+            groups_colors = [mapping[f] for f in features]
+        return (groups_colors, data)
 
     def add_features(self, features, numeric=False, control=False):
         self._sanity_check(features, feature_name='features')
@@ -69,6 +105,13 @@ class LingMap(object):
             self.numeric = False
         if control:
             self.control = True
+
+    def add_stroke_features(self, features, numeric=False):
+        self.stroke_features = features
+        if numeric:
+            self.s_numeric = True
+        else:
+            self.s_numeric = False
 
     def add_popups(self, popups):
         self._sanity_check(popups, feature_name='popups')
@@ -89,43 +132,30 @@ class LingMap(object):
     def _create_map(self):
         m = folium.Map(location=[0, 0], zoom_start=3)
         if 'features' in dir(self):
-            if self.numeric:
-                features = self.features
-                features.sort()
-                colormap = branca.colormap.LinearColormap(colors=['#e6ccff','#4a008f'], index=[features[0],features[-1]], vmin=features[0], vmax=features[-1])
-                groups_colors = [(0, colormap(feature)) for feature in features]
-            else:
-                mapping = {}
-                legend_data = ''
-                clear_features = []
-                groups = []
-                for i, feature in enumerate(self.features):
-                    if feature not in clear_features:
-                        clear_features.append(feature)
-                        groups.append(folium.FeatureGroup(name=self.features[i]))
-                for i, feature in enumerate(clear_features):
-                    mapping[feature] = (groups[i], self.colors[i])
-                    legend_data += '<li><span style="background: {};opacity:0.7;"></span>{}</li>\n'.format(self.colors[i], feature)
-                groups_colors = [mapping[f] for f in self.features]
+            prepared = self._prepare_features(self.features)
+            groups_colors = prepared[0]
+            data = prepared[1]
+
+        if 'stroke_features' in dir(self):
+            prepared = self._prepare_features(self.stroke_features, stroke=True)
+            s_groups_colors = prepared[0]
+            s_data = prepared[1]
         
         for i, language in enumerate(self.languages):
             if 'custom_coordinates' in dir(self):
                 coordinates = self.custom_coordinates[i]
             else:
                 coordinates = self._get_coordinates(language)
+                
             if 'features' in dir(self):
                 color = groups_colors[i][1]
             else:
                 color = '#DEB887'
-            marker = folium.CircleMarker(
-                        location=[coordinates[0], coordinates[1]],
-                        radius=7,
-                        fill=True,
-                        weight=1,
-                        fill_opacity=1,
-                        color='#000000',
-                        fill_color=color
-                    )
+                
+            if 'stroke_features' in dir(self):
+                marker = self._set_marker([coordinates[0], coordinates[1]], stroke=True, weight=3, fill_color=color, color=s_groups_colors[i][1])
+            else:
+                marker = self._set_marker([coordinates[0], coordinates[1]], stroke=True, fill_color=color)
             
             self._create_popups(marker, language, i)
 
@@ -139,12 +169,15 @@ class LingMap(object):
     
         if 'features' in dir(self):
             if self.numeric:
+                colormap = data
                 m.add_child(colormap)
             else:
                 if self.control:
                     [m.add_child(fg[0]) for fg in groups_colors]
                     folium.LayerControl(collapsed=False).add_to(m)
-                self._create_legend(m, legend_data)
+                self._create_legend(m, data)
+                if 'stroke_features' in dir(self):
+                    self._create_legend(m, s_data, position='left')
         return m
 
     def save(self, path):
@@ -177,6 +210,7 @@ def circassian_test():
 
     m = LingMap(languages)
     m.add_features(features, control=True)
+    m.add_stroke_features(languages)
     m.add_popups(popups)
     m.add_tooltips(languages)
     m.add_custom_coordinates(coordinates)
@@ -192,6 +226,6 @@ def ejectives_test():
     #m.languages_in_popups = False
     m.save('ejectives.html')
 
-#random_test()
-#circassian_test()
-#ejectives_test()
+random_test()
+circassian_test()
+ejectives_test()
