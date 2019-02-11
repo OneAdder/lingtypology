a1f8fca150f5bc555db941c0e64ba12dac47bf0b
OneAdder <mikivo@list.ru>
2019-02-11 21:32:14 +0300

    Пузырики без границ друг между другом (главный секрет Гарика)

--- typological_map.py
+++ typological_map.py
@@ -6,7 +6,6 @@ import branca.element
 
 import jinja2
 import pandas
-import pygal
 
 legend_html = ''
 csv = pandas.read_csv('glottolog.csv', delimiter='\t', header=0)
@@ -33,6 +32,7 @@ class LingMap(object):
     start_location = (0, 0)
     start_zoom = 3
     minimap = {}
+    colormap_colors = ('#e6ccff','#4a008f')
     languages_in_popups = True
     use_shapes = False
     control_scale = True
@@ -47,6 +47,9 @@ class LingMap(object):
     stroke_legend_position = 'bottomleft'
     rectangles = []
     lines = []
+    html_popups = False
+    radius = 7
+    stroke_radius = 12
     
     def __init__(self, languages):
         self.languages = languages
@@ -59,24 +62,33 @@ class LingMap(object):
     def _get_glot_id(self,language):
         return tuple(csv[csv.language == language].glottocode)[0]
 
-    def _create_popups(self, marker, language, i):
+    def _create_popups(self, marker, language, i, parse_html=False):
         popup_href = '''<a href="https://glottolog.org/resource/languoid/id/{}" onclick="this.target='_blank';">{}</a><br>'''
         if self.languages_in_popups:
             if 'popups' in dir(self):
+                if parse_html:
+                    raise LingMapError('''
+                                       It is impossible to add both language links and large HTML strings.
+                                       You can either not use parse_html option or set ling_map_object.languages_in_popups = False.
+                                       ''')
                 popup = folium.Popup(popup_href.format(self._get_glot_id(language), language) + self.popups[i])
             else:
                 popup = folium.Popup(popup_href.format(self._get_glot_id(language), language))
             popup.add_to(marker)
         else:
             if 'popups' in dir(self):
-                popup = folium.Popup(self.popups[i])
+                if parse_html:
+                    iframe = folium.IFrame(html=self.popups[i])
+                    popup = folium.Popup(iframe)
+                else:
+                    popup = folium.Popup(self.popups[i])
                 popup.add_to(marker)
 
     def _create_legend(self, m, legend_data, title='Legend', position='bottomright'):
         with open('legend.html', 'r', encoding='utf-8') as f:
             template = f.read()
         template = jinja2.Template(template)
-        template = template.render(data=legend_data, position=position, title=title)
+        template = template.render(data=legend_data, position=position, title=title, use_shapes=self.use_shapes)
         template = '{% macro html(this, kwargs) %}' + template + '{% endmacro %}'
         macro = branca.element.MacroElement()
         macro._template = branca.element.Template(template)
@@ -93,7 +105,7 @@ class LingMap(object):
                     fill_color='#DEB887',
                     shape=''):
         if shape:
-            div = folium.DivIcon(html=('<div style="font-size: 170%">' + shape + '</div>'))
+            div = folium.DivIcon(html=('<div style="font-size: 170%">' + str(shape) + '</div>'))
             marker = folium.Marker(location=location, icon=div)
         else:
             marker = folium.CircleMarker(
@@ -115,7 +127,7 @@ class LingMap(object):
             colors.reverse()
         if self.numeric and not stroke:
             features.sort()
-            colormap = branca.colormap.LinearColormap(colors=['#e6ccff','#4a008f'], index=[features[0],features[-1]], vmin=features[0], vmax=features[-1])
+            colormap = branca.colormap.LinearColormap(colors=self.colormap_colors, index=[features[0],features[-1]], vmin=features[0], vmax=features[-1])
             groups_features = [(0, colormap(feature)) for feature in features]
             data = colormap
         else:
@@ -129,13 +141,17 @@ class LingMap(object):
                     groups.append(folium.FeatureGroup(name=features[i]))
             for i, feature in enumerate(clear_features):
                 mapping[feature] = (groups[i], colors[i])
-                data += '<li><span style="background: {};opacity:0.7;"></span>{}</li>\n'.format(self.colors[i], feature)
+                if use_shapes:
+                    data += '<li><span style="color: #000000; text-align: center; opacity:0.7;">{}</span>{}</li>\n'.format(colors[i], feature)
+                else:
+                    data += '<li><span style="background: {};opacity:0.7;"></span>{}</li>\n'.format(colors[i], feature)
             groups_features = [mapping[f] for f in features]
         return (groups_features, data)
 
-    def add_features(self, features, numeric=False, control=False, use_shapes=False):
+    def add_features(self, features, radius=7, numeric=False, control=False, use_shapes=False):
         self._sanity_check(features, feature_name='features')
         self.features = features
+        self.radius = 7
         if numeric:
             self.numeric = True
         else:
@@ -145,7 +161,7 @@ class LingMap(object):
         if use_shapes:
             self.use_shapes = True
 
-    def add_stroke_features(self, features, numeric=False, control=False):
+    def add_stroke_features(self, features, radius=12, numeric=False, control=False):
         self._sanity_check(features, feature_name='stroke features')
         self.stroke_features = features
         if numeric:
@@ -154,10 +170,13 @@ class LingMap(object):
             self.s_numeric = False
         if control:
             self.stroke_control = True
+        self.stroke_radius = 12
 
-    def add_popups(self, popups):
+    def add_popups(self, popups, parse_html=False):
         self._sanity_check(popups, feature_name='popups')
         self.popups = popups
+        if parse_html:
+            self.html_popups=True
 
     def add_tooltips(self, tooltips):
         self._sanity_check(tooltips, feature_name='tooltips')
@@ -186,6 +205,13 @@ class LingMap(object):
     
     def _create_map(self):
         m = folium.Map(location=self.start_location, zoom_start=self.start_zoom, control_scale=self.control_scale)
+        default_group = folium.FeatureGroup()
+        markers = []
+        stroke_markers = []
+        s_markers = []
+        s_stroke_markers = []
+        s_marker = ''
+        s_stroke_marker = ''
         if 'features' in dir(self):
             prepared = self._prepare_features(self.features, use_shapes=self.use_shapes)
             groups_features = prepared[0]
@@ -197,6 +223,9 @@ class LingMap(object):
             s_data = prepared[1]
         
         for i, language in enumerate(self.languages):
+            stroke_marker = False
+            radius = self.radius
+            stroke_radius = radius * 1.15
             if 'custom_coordinates' in dir(self):
                 coordinates = self.custom_coordinates[i]
             else:
@@ -208,43 +237,67 @@ class LingMap(object):
                 color_shape = '#DEB887'
                 
             if 'stroke_features' in dir(self):
-                marker = self._set_marker([coordinates[0], coordinates[1]], stroke=True, weight=3, fill_color=color_shape, color=s_groups_features[i][1])
+                marker = self._set_marker([coordinates[0], coordinates[1]], fill_color=color_shape)
+                stroke_marker = self._set_marker([coordinates[0], coordinates[1]], stroke=True, radius=radius*1.15, fill_color='#000000')
+                s_marker = self._set_marker([coordinates[0], coordinates[1]], radius=self.stroke_radius, fill_color=s_groups_features[i][1])
+                s_stroke_marker = self._set_marker([coordinates[0], coordinates[1]], stroke=True, radius=self.stroke_radius*1.15, fill_color='#000000')
             else:
                 if self.use_shapes:
-                    marker = self._set_marker([coordinates[0], coordinates[1]], stroke=True, fill_color='#000000', shape=color_shape)
+                    marker = self._set_marker([coordinates[0], coordinates[1]], fill_color='#000000', shape=color_shape)
                 else:
-                    marker = self._set_marker([coordinates[0], coordinates[1]], stroke=True, fill_color=color_shape)
+                    marker = self._set_marker([coordinates[0], coordinates[1]], radius=radius, fill_color=color_shape)
+                    stroke_marker = self._set_marker([coordinates[0], coordinates[1]], stroke=True, radius=stroke_radius, fill_color='#000000')
             
-            self._create_popups(marker, language, i)
+            self._create_popups(marker, language, i, parse_html=self.html_popups)
 
             if 'features' in dir(self) and not self.numeric and self.control:
-                marker.add_to(groups_features[i][0])
+                group = groups_features[i][0]
             elif 'stroke_features' in dir(self) and self.stroke_control:
-                marker.add_to(s_groups_features[i][0])
+                group = s_groups_features[i][0]
             else:
-                marker.add_to(m)
+                group = default_group
                 
             if 'tooltips' in dir(self):
                 tooltip = folium.Tooltip(self.tooltips[i])
                 tooltip.add_to(marker)
+
+            markers.append((marker, group))
+            if stroke_marker:
+                stroke_markers.append((stroke_marker, group))
+            if s_marker:
+                s_markers.append((s_marker, group))
+            if s_stroke_marker:
+                s_stroke_markers.append((s_stroke_marker, group))
+
+        if s_stroke_markers:
+            [s_stroke_mark[0].add_to(s_stroke_mark[1]) for s_stroke_mark in s_stroke_markers]
+        if s_markers:
+            [s_mark[0].add_to(s_mark[1]) for s_mark in s_markers]
+        if stroke_markers:
+            [stroke_mark[0].add_to(stroke_mark[1]) for stroke_mark in stroke_markers]
+        [mark[0].add_to(mark[1]) for mark in markers]
     
         if 'features' in dir(self):
             if self.numeric:
+                m.add_child(default_group)
                 colormap = data
                 m.add_child(colormap)
             else:
-                
                 if self.control:
                     [m.add_child(fg[0]) for fg in groups_features]
                     folium.LayerControl(collapsed=False, position=self.control_position).add_to(m)
                 elif self.stroke_control:
                     [m.add_child(fg[0]) for fg in s_groups_features]
                     folium.LayerControl(collapsed=False, position=self.control_position).add_to(m)
+                else:
+                    m.add_child(default_group)
                 
                 if self.legend:
                     self._create_legend(m, data, title=self.legend_title, position=self.legend_position)
                 if 'stroke_features' in dir(self) and self.stroke_legend:
                     self._create_legend(m, s_data, title=self.stroke_legend_title, position=self.stroke_legend_position)
+        else:
+            m.add_child(default_group)
         if self.minimap:
             minimap = folium.plugins.MiniMap(**self.minimap)
             m.add_child(minimap)
@@ -254,7 +307,6 @@ class LingMap(object):
         if self.lines:
             for line in self.lines:
                 folium.PolyLine(**line).add_to(m)
-
         return m
 
     def save(self, path):
@@ -291,7 +343,7 @@ def circassian_test():
     m = LingMap(languages)
     m.start_location = (44.21, 42.32)
     m.start_zoom = 8
-    m.add_features(dialects, control=True)
+    m.add_features(dialects)#, control=True)
     #m.control_position = 'bottomleft'
     m.add_stroke_features(languages)
     m.add_popups(popups)
@@ -318,29 +370,32 @@ def ejectives_test():
     #m.languages_in_popups = False
     m.save('ejectives.html')
 
-def ejectives2_test():
-    data = pandas.read_csv('ejective_and_n_consonants.csv', delimiter=',', header=0)
-    languages = list(data.language)
-    vowels = list(data.vowels)
-    consonants = list(data.consonants)
-    
-    m = LingMap(languages)
-    #m.add_minicharts(zip(vowels, consonants))
-    m.save('ejectives2.html')
 
-def ejectives3_test():
-    data = pandas.read_csv('ejective_and_n_consonants.csv', delimiter=',', header=0)
-    languages = list(data.language)
+def circassian2_test():
+    circassian = pandas.read_csv('circassian.csv', delimiter=',', header=0)
+
+    coordinates = list(zip(list(circassian.latitude), list(circassian.longitude)))
+    dialects = list(circassian.dialect)
+
+    languages = list(circassian.language)
+    popups = list(circassian.village)
 
     m = LingMap(languages)
-    m.add_features(languages)
-    m.save('ejectives3_test.html')
+    #m.shapes = range(1, 20)
+    m.start_location = (44.21, 42.32)
+    m.start_zoom = 8
+    m.add_custom_coordinates(coordinates)
+    m.add_features(dialects, use_shapes=True)
+    m.save('circassian2_test.html')
 
-#random_test()
-#circassian_test()
-#ejectives_test()
-#ejectives2_test()
-#ejectives3_test()
+def simplest_test():
+    m = LingMap(('Romanian', 'Ukrainian'))
+    m.save('simplest_test.html')
 
+simplest_test()
+random_test()
+circassian_test()
+ejectives_test()
+circassian2_test()
 
 
