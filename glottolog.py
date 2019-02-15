34bbe8c068c79333d15b3c8cc2c88db55758f8ba
OneAdder <mikivo@list.ru>
2019-02-15 16:50:42 +0300

    support for new table

--- glottolog.py
+++ glottolog.py
@@ -1,20 +1,25 @@
 import pandas
 
-glottolog_original = pandas.read_csv('glottolog.csv', delimiter='\t', header=0)
+glottolog = pandas.read_csv('glottolog.csv', delimiter=',', header=0)
 
 '''
 get_affiliations(('Russian', 'English'))
->>> ['Indo-European, Slavic, East', 'Indo-European, Germanic, West, English']
+>>> ['Indo-European, Balto-Slavic, Slavic, East Slavic', 'Indo-European, ...']
 '''
 def get_affiliations(languages):
     affiliations = []
     for language in languages:
-        affiliation = tuple(glottolog_original[glottolog_original.language == language].affiliation)
-        if not affiliation:
+        affiliation_id = tuple(glottolog[glottolog.Name == language].Classification)
+        if not affiliation_id:
+            affiliation = ''
             print('(get_affiliations) Warning: affiliation for {} not found'.format(language))
-            affiliations.append('')
         else:
-            affiliations.append(affiliation[0])
+            affiliation = []
+            affiliation_id = affiliation_id[0].split('/')
+            for taxon in affiliation_id:
+                affiliation.append(tuple(glottolog[glottolog.ID == taxon].Name)[0])
+            affiliation = ', '.join(affiliation)
+        affiliations.append(affiliation)
     return affiliations
 
 '''
@@ -22,19 +27,20 @@ get_coordinates('Russian')
 >>> (59.0, 50.0)
 '''
 def get_coordinates(language):
-    latitude = glottolog_original[glottolog_original.language == language].latitude
-    longitude = glottolog_original[glottolog_original.language == language].longitude
+    latitude = glottolog[glottolog.Name == language].Latitude
+    longitude = glottolog[glottolog.Name == language].Longitude
     if not list(latitude) or not list(longitude):
         print('(get_coordinates) Warning: coordinates for {} not found'.format(language))
     else:
         return (float(latitude), float(longitude))
 
+
 '''
-get_glot_id(language)
+get_glot_id('Russian')
 >>> russ1263
 '''
 def get_glot_id(language):
-    glot_id = tuple(glottolog_original[glottolog_original.language == language].glottocode)
+    glot_id = tuple(glottolog[glottolog.Name == language].ID)
     if not glot_id:
         print('(get_glot_id) Warning: Glottolog ID for {} not found'.format(language))
     else:
@@ -45,29 +51,19 @@ get_macro_area('Russian')
 >>> Eurasia
 '''
 def get_macro_area(language):
-    macro_area = tuple(glottolog_original[glottolog_original.language == language].area)
+    macro_area = tuple(glottolog[glottolog.Name == language].Macroarea)
     if not macro_area:
         print('(get_macro_area) Warning: Macro area for {} not found'.format(language))
     else:
         return macro_area[0]
 
-'''
-get_country('Russian')
->>> Canada, Czech Republic, Lithuania, Mongolia, ...
-'''
-def get_country(language):
-    country = tuple(glottolog_original[glottolog_original.language == language].country)
-    if not country:
-        print('(get_country) Warning: country for {} not found'.format(language))
-    else:
-        return country[0]
 
 '''
 get_iso('Russian')
 >>> rus
 '''
 def get_iso(language):
-    iso = tuple(glottolog_original[glottolog_original.language == language].iso)
+    iso = tuple(glottolog[glottolog.Name == language].ISO639P3code)
     if not iso:
         print('(get_iso) Warning: ISO for {} not found'.format(language))
     else:
@@ -78,20 +74,20 @@ def get_iso(language):
 '''
 get_by_affiliation('Indo-European, Slavic, East')
 >>> ('Ukrainian', 'Rusyn', 'Russian', 'Belarusian')
-'''
 def get_by_affiliation(affiliation):
-    languages = tuple(glottolog_original[glottolog_original.affiliation == affiliation].language)
+    languages = tuple(glottolog[glottolog.affiliation == affiliation].language)
     if not languages:
         print('(get_by_affiliation) Warning: languages by {} not found'.format(affiliation))
     else:
         return languages
+'''
 
 '''
 get_by_iso('rus')
 >>> Russian
 '''
 def get_by_iso(iso):
-    language = tuple(glottolog_original[glottolog_original.iso == iso].language)
+    language = tuple(glottolog[glottolog.ISO639P3code == iso].Name)
     if not language:
         print('(get_by_iso) Warning: language by {} not found'.format(iso))
     else:
@@ -102,7 +98,7 @@ get_by_glot_id('russ1263')
 >>> Russian
 '''
 def get_by_glot_id(glot_id):
-    language = tuple(glottolog_original[glottolog_original.glottocode == glot_id].language)
+    language = tuple(glottolog[glottolog.ID == glot_id].Name)
     if not language:
         print('(get_by_glot_id) Warning: language by {} not found'.format(glot_id))
     else:
@@ -115,7 +111,7 @@ get_glot_id_by_iso('rus')
 >>> russ1263
 '''
 def get_glot_id_by_iso(iso):
-    glot_id = tuple(glottolog_original[glottolog_original.iso == iso].glottocode)
+    glot_id = tuple(glottolog[glottolog.ISO639P3code == iso].ID)
     if not glot_id:
         print('(get_glot_id_by_iso) Warning: glot_id by {} not found'.format(iso))
     else:
@@ -126,9 +122,8 @@ get_iso_by_glot_id('russ1263')
 >>> rus
 '''
 def get_iso_by_glot_id(glot_id):
-    iso = tuple(glottolog_original[glottolog_original.glottocode == glot_id].iso)
+    iso = tuple(glottolog[glottolog.ID == glot_id].ISO639P3code)
     if not iso:
         print('(get_iso_by_glot_id) Warning: ISO by {} not found'.format(iso))
     else:
         return iso[0]
-
