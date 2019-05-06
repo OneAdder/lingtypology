d1cbb5f789cc8940a623ddb54a12c4d5a640800b
OneAdder <mikivo@list.ru>
2019-05-06 22:08:41 +0300

    better wals

--- lingtypology/db_apis.py
+++ lingtypology/db_apis.py
@@ -1,8 +1,10 @@
-"""APIs for work Wals and Phoible. Requires Internet connection."""
+"""APIs for work Wals and Autotyp."""
 import pandas
-import urllib.request as ur
+import urllib
 import lingtypology.glottolog
+import warnings
 
+ur = urllib.request
 
 class Wals(object):
     """Wals
@@ -52,7 +54,7 @@ class Wals(object):
         try:
             df_feature = pandas.read_csv(wals_url, delimiter='\t', skiprows=7)
         except:
-            print('(Wals) Warning: cannot read Wals feature ' + feature)
+            warnings.warn('(Wals) Warning: cannot read Wals feature ' + feature)
         else:
             return df_feature[['wals code','description']]
 
@@ -65,7 +67,11 @@ class Wals(object):
         Returns str
         """
         wals_url = 'http://wals.info/feature/{}.tab'.format(feature)
-        wals_page = ur.urlopen(wals_url)
+        try:
+            wals_page = ur.urlopen(wals_url)
+        except urllib.error.HTTPError:
+            warnings.warn('No such feature in WALS')
+            return
         _citation = 'Citation for feature {}:\n{}\n'
         citation = _citation.format(feature, '\n'.join(wals_page.read().decode('utf-8').split('\n')[:5]))
         return citation
@@ -84,10 +90,13 @@ class Wals(object):
         for feature in features:
             feature = feature.upper()
             if self.show_citation:
-                print(self._get_citation(feature))
+                citation = self._get_citation(feature)
+                if citation:
+                    print(citation)
             wals_feature = self._get_wals_data(feature)
-            wals_feature = wals_feature.rename(columns={'description': '_' + feature})
-            df = pandas.merge(df, wals_feature, on="wals code")
+            if not wals_feature is None:
+                wals_feature = wals_feature.rename(columns={'description': '_' + feature})
+                df = pandas.merge(df, wals_feature, on="wals code")
         return df
 
     def get_json(self):
@@ -101,57 +110,6 @@ class Wals(object):
         js = { header:list(df[header]) for header in list(df)}
         return js
 
-'''
-class Phoible(object):
-    """Phoible
 
-    show_citation: bool, default True
-        Whether to print the citation.
-    """
-    show_citation = True
-    citation =  """
-Moran, Steven & McCloy, Daniel & Wright, Richard (eds.) 2014. PHOIBLE Online. Leipzig: Max Planck Institute for Evolutionary Anthropology. (Available online at http://phoible.org, Accessed on ...)
-A BibTeX entry for LaTeX users is
-@book{phoible,
-address   = {Leipzig},
-editor    = {Steven Moran and Daniel McCloy and Richard Wright},
-publisher = {Max Planck Institute for Evolutionary Anthropology},
-title     = {PHOIBLE Online},
-url       = {http://phoible.org/},
-year      = {2014}
-}
-                """
-    def get_df(self):
-        """Get data from Phoible in pandas.DataFrame format.
-
-        Returns pandas.DataFrame
-            Headers: 'iso', 'language', 'coordinates', 'phonemes', 'consonants', 'tones', 'vowels'.
-        """
-        if self.show_citation:
-            print(self.citation)
-        phoible_url = 'https://raw.githubusercontent.com/clld/phoible/master/data/phoible-aggregated.tsv'
-        original_df = pandas.read_csv(phoible_url, delimiter='\t', header=0)
-        
-        df = pandas.DataFrame(columns=['iso', 'language', 'coordinates', 'phonemes', 'consonants', 'tones', 'vowels'])
-        df.iso = original_df.LanguageCode
-        df.language = [glottolog.get_by_iso(iso) for iso in list(original_df.LanguageCode)]
-        df.coordinates = zip(list(original_df.Latitude), list(original_df.Longitude))
-        df.phonemes = original_df.Phonemes.astype(int)
-        df.consonants = original_df.Consonants.astype(int)
-        df.tones = original_df.Tones.astype(int)
-        df.vowels = original_df.Vowels.astype(int)
-        return df
-
-    def get_json(self):
-        """Get data from Phoible in pandas.DataFrame format.
-
-        Returns dict
-            Keys: 'iso', 'language', 'coordinates', 'phonemes', 'consonants', 'tones', 'vowels'.
-        """
-        df = self.get_df()
-        js = {header: list(df[header]) for header in list(df)}
-        return js
-'''
-
-#print(Wals('1a').get_json())
+#print(Wals('1af', '2a').get_df())
 #print(list(Phoible().get_json()))
