c5162587dd4e7a88f9d8507ccb215b14602c3ce1
OneAdder <mikivo@list.ru>
2019-06-03 21:02:42 +0300

    long description for PyPi

--- setup.py
+++ setup.py
@@ -1,3 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+# lingtypology
+Lingtypology is a Python3 tool for linguistic interactive mapping and online linguistic databases API.
+It is inspired by [R-package](https://github.com/ropensci/lingtypology) created by [Agrizolamz](https://github.com/agricolamz).
+It uses the same phylosophy and provides similar functionality.  
+
+## Installation
+The package is available in PyPI.
+Therefore, you can install it by running:  
+Locally: `pip3 install lingtypology --user`  
+Globally (not recommended): `sudo pip3 install lingtypology`
+
+## Usage
+Lingtypology package contains `LingMap` class that allows to draw interactive maps, `glottolog` library that allows to interact with Glottolog data and `db_apis` that allows to interact with different other linguistic databases.  
+For more informations consult the [tutorial](https://oneadder.github.io/lingtypology/).
+"""
 from setuptools import setup
 from lingtypology import __version__
 
@@ -5,6 +22,8 @@ setup(
     name='lingtypology',
     version=__version__,
     description='A tool for linguistic typology.',
+    long_description=__doc__,
+    long_description_content_type='text/markdown',
     url='https://github.com/OneAdder/lingtypology',
     author='Michael Voronov',
     author_email='mikivo@list.ru',
