481c87f405a4d86565ee4f03a4e6d53a66e41e83
OneAdder <mikivo@list.ru>
2019-06-03 20:38:34 +0300

    citation info

--- lingtypology/__init__.py
+++ lingtypology/__init__.py
@@ -2,3 +2,12 @@ from lingtypology.maps import *
 import lingtypology.db_apis
 
 __version__ = '0.8.1'
+__citation__ = \
+    '@misc{MichaelVoronov2669068,\n' \
+    '    author = {Michael Voronov},\n' \
+    '    title = {{lingtypology: a Python tool for linguistic interactive mapping}},\n' \
+    '    month = june,\n' \
+    '    year = 2019,\n' \
+    '    doi = {10.5281/zenodo.2669068},\n' \
+    '    url = {https://doi.org/10.5281/zenodo.2669068}\n' \
+    '}'
