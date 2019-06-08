cf2d743a844ade90619117b6acb12d9abd35d529
OneAdder <mikivo@list.ru>
2019-06-08 12:28:10 +0300

    copyleft note

--- lingtypology/__init__.py
+++ lingtypology/__init__.py
@@ -11,3 +11,22 @@ __citation__ = \
     '    doi = {10.5281/zenodo.2669068},\n' \
     '    url = {https://doi.org/10.5281/zenodo.2669068}\n' \
     '}'
+
+__doc__ = \
+'''
+lingtypology: a Python tool for linguistic typology.
+Copyright (C) 2019  Michael Voronov
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+'''
